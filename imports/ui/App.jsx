203885dbf536fd244206746fee9dee53ed52873b
Nick Engeldinger <dbozmaxima@gmail.com>
2017-05-14 19:48:46 -0400

    Added some routes, reorganized structure for route and index startup code

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-export default class Home extends Component {
+export default class App extends Component {
 	render() {
 		return(
-			<div>
-				<h1>Ducks Kicks</h1>
-				<Link to="/kicks">All Kicks</Link>
+			<div className="app">
+				<header className="primary-header">
+					<h1>Ducks Kicks</h1>
+					<img className="logo" src="logo.png" />
+				</header>
+				<aside className="primary-aside">
+					<Link to="/">Home</Link>
+					<Link to="/kicks">All Kicks</Link>
+					<Link to="/about">About</Link>
+				</aside>
+				{this.props.children}
 			</div>
 		)
 	}
--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-export default class NotFound extends Component {
+export default class Home extends Component {
 	render() {
-		return (
-			<div>
-				<h2>Sorry there's nothing here</h2>
+		return(
+			<div className="home">
+				<h2>Home</h2>
 			</div>
-		);
+		)
 	}
 }
