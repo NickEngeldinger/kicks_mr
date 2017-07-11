fc9d3953ae1370310f0f3f5531664006f8bd0272
Nick Engeldinger <dbozmaxima@gmail.com>
2017-07-10 22:35:09 -0400

    Added form to add new sneakers into the collection

--- imports/ui/Kicks.jsx
+++ imports/ui/Kicks.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
@@ -7,26 +8,47 @@ import { Kicks } from '../api/kicks.js';
 import Kick from './Kick.jsx';
 
 class KicksApp extends Component {
-	/*getKicks() {
-		return [
-			{ _id: 1, model: 'Jordan 4', colorway: 'white', category: 'retro' },
-			{ _id: 2, model: 'Jordan 3 Pit Crew', colorway: 'black', category: 'retro' },
-			{ _id: 3, model: 'Nike Zoom Speed Turf', colorway: 'camo', category: 'football' }
-		];
-	}*/
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		const model = ReactDOM.findDOMNode(this.refs.modelInput).value.trim();
+		const colorway = ReactDOM.findDOMNode(this.refs.colorwayInput).value.trim();
+
+		Kicks.insert({
+			model,
+			colorway,
+			createdAt: new Date()
+		});
+
+		ReactDOM.findDOMNode(this.refs.modelInput).value = '';
+		ReactDOM.findDOMNode(this.refs.colorwayInput).value = '';
+	}
 
 	renderKicks() {
 		return this.props.kicks.map((kick) => (
 			<Kick key={kick._id} kick={kick} />
 		));
-		//return this.getKicks().map((kick) => (
-		//	<Kick key={kick._id} kick={kick} />
-		//));
 	}
 
 	render() {
 		return (
 			<div className="container">
+
+				<form className="new-kick" onSubmit={this.handleSubmit.bind(this)} >
+					<input
+						type="text"
+						ref="modelInput"
+						placeholder="Model of sneaker"
+					/>
+					<input
+						type="text"
+						ref="colorwayInput"
+						placeholder="Colorway of sneaker"
+					/>
+					<button type="submit">Add Sneaker</button>
+				</form>	
+
 				<ul>
 						{this.renderKicks()}
 				</ul>
