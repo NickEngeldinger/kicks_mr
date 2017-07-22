12044770f0f7cd3a3626b51bde4d94e1d1f80dd4
Nick Engeldinger <dbozmaxima@gmail.com>
2017-07-22 18:33:22 -0400

    The Admin component now uses createContainer so the authentication data of a user is reactive and Admin has a currentUser prop containing the Meteor.user data

--- imports/ui/Admin.jsx
+++ imports/ui/Admin.jsx
@@ -1,23 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
 
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 import AddKick from './AddKick.jsx';
 
-export default class Admin extends Component {
-
+class Admin extends Component {
 	render() {
 		return (
 			<div>
 				<AccountsUIWrapper />
 
-				{ Meteor.userId() ?
+				{ this.props.currentUser ?
 				<div>
 					<h2>Admin</h2>
 
 					<AddKick />
+
 				</div>
 				: ''}
 			</div>
 		);
 	}
 }
+
+Admin.propTypes = {
+	currentUser: PropTypes.object
+}
+
+export default createContainer(() => {
+	return {currentUser : Meteor.user()};	
+}, Admin);
